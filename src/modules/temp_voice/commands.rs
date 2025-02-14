de020661b3ab54d10f9d874089b24367e104e997
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-02-14 02:09:13 +0000

    Add privacy check to voice command interaction response

--- src/modules/temp_voice/commands.rs
+++ src/modules/temp_voice/commands.rs
@@ -1,5 +1,8 @@
 use async_trait::async_trait;
-use serenity::all::{CommandInteraction, Context, CreateCommand, Ready, ResolvedOption};
+use serenity::all::{
+    CommandInteraction, Context, CreateCommand, CreateInteractionResponse,
+    CreateInteractionResponseMessage, Ready, ResolvedOption,
+};
 use sqlx::{PgPool, Postgres};
 use zayden_core::SlashCommand;
 
@@ -15,9 +18,35 @@ impl SlashCommand<Error, Postgres> for Voice {
     async fn run(
         ctx: &Context,
         interaction: &CommandInteraction,
-        _options: Vec<ResolvedOption<'_>>,
+        options: Vec<ResolvedOption<'_>>,
         pool: &PgPool,
     ) -> Result<()> {
+        let Some(ResolvedOption { name, .. }) = options.first() else {
+            unreachable!();
+        };
+
+        if *name == "privacy"
+            && !interaction
+                .member
+                .as_ref()
+                .unwrap()
+                .permissions
+                .unwrap()
+                .manage_channels()
+        {
+            interaction
+                .create_response(
+                    ctx,
+                    CreateInteractionResponse::Message(
+                        CreateInteractionResponseMessage::new()
+                            .content("This command is not enabled on this server."),
+                    ),
+                )
+                .await
+                .unwrap();
+            return Ok(());
+        }
+
         temp_voice::VoiceCommand::run::<Postgres, GuildTable, VoiceChannelTable>(
             ctx,
             interaction,
